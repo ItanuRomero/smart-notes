8d8e7a104962e19258f5173cd2892d4fcced977e
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2022-03-25 18:05:40 -0300

    now each user can access only its notes

--- src/notes/views.py
+++ src/notes/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
 from django.http import Http404
 from django.views.generic import CreateView, ListView, DetailView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import NotesForm
 from .models import Notes
@@ -15,17 +17,26 @@ class NotesCreateView(CreateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
-
+    
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+        
 class NotesUpdateView(UpdateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
 
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = "notes"
     template_name = "notes/notes_list.html"
-   
+    login_url = "/admin"
+    
+    def get_queryset(self):
+        return self.request.user.notes.all()
  
 class NotesDetailView(DetailView):
     model = Notes
