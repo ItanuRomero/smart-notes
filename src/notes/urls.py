1093d95a8377beb556536446269e097006648293
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2022-03-25 17:48:00 -0300

    finish crud with delete operation

--- src/notes/urls.py
+++ src/notes/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('notes', views.NotesListView.as_view(), name='notes.list'),
     path('notes/<int:pk>', views.NotesDetailView.as_view(), name='notes.detail'),
     path('notes/<int:pk>/edit', views.NotesUpdateView.as_view(), name='notes.update'),
-    path('notes/new', views.NotesCreateView.as_view(), name='notes.new')
+    path('notes/<int:pk>/delete', views.NotesDeleteView.as_view(), name='notes.delete'),
+    path('notes/new', views.NotesCreateView.as_view(), name='notes.new'),
+    
 ]
--- src/notes/views.py
+++ src/notes/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import Http404
 from django.views.generic import CreateView, ListView, DetailView, UpdateView
+from django.views.generic.edit import DeleteView
 
 from .forms import NotesForm
 from .models import Notes
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = 'notes/notes_delete.html'
 
 class NotesCreateView(CreateView):
     model = Notes
