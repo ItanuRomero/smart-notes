d2c687833974132918119a76d620766b4fcefb60
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2022-03-25 14:58:44 -0300

    changing our views using class-based views

--- src/home/views.py
+++ src/home/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
-from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = 'welcome.html'
+    extra_context = {'today': datetime.today()}
 
-def home(request):
-    return render(request, 'welcome.html', {'today': datetime.today()})
 
-@login_required(login_url='/admin')
-def authorized(request):
-    return render(request, 'home/authorized.html', {})
+class AuthorizedView(LoginRequiredMixin, TemplateView):
+    template_name = 'home/authorized.html'
